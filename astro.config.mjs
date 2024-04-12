0b15b4f9c1af7f428e56af6492ffe4283b089547
sonvice <wvmc_es@hotmail.com>
2024-04-12 20:32:06 +0200

    Add Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://formatossociales.com',
-  integrations: [sitemap()]
+  integrations: [sitemap(),partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),]
 });
